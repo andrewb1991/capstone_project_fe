01ea5fa79ddc5b3d6c97bc8671d4d5845689e0c3
andrewb1991 <andrea.bramucci@gmail.com>
2023-04-13 22:20:53 +0200

    modifica

--- src/views/products/ManageProductsList.js
+++ src/views/products/ManageProductsList.js
@@ -3,6 +3,7 @@ import useFetch from "./useFetch";
 import axios from 'axios';
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
+import { useParams } from "react-router";
 import {
   MDBBadge,
   MDBBtn,
@@ -22,13 +23,14 @@ import {
   MDBCheckbox,
 } from "mdb-react-ui-kit";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ProductEditModal from "./ProductEditModal";
 
-const ManageProductsList = () => {
+const ManageProductsList = (props) => {
+  const { id } = useParams()
     const navigate = useNavigate();
   const [fullscreenXlModal, setFullscreenXlModal] = useState(false);
   const toggleShow = () => setFullscreenXlModal(!fullscreenXlModal);
   const [newProductFrom, setNewProductForm] = useState({});
-  const [isloading, setLoading] = useState(false);
 
   const handleBackPage = () => {
   navigate("/homeemployee")
@@ -167,6 +169,8 @@ const ManageProductsList = () => {
                         />
                         <div className="ms-3">
                           <p className="fw-bold mb-1">{product.product}</p>
+                          <p className="text-muted mb-0">id: {product._id}</p>
+
                         </div>
                       </div>
                     </td>
@@ -177,9 +181,73 @@ const ManageProductsList = () => {
                       <p className="fw-normal mb-1">{product.price} €</p>
                     </td>
                     <td>
-                      <MDBBtn color="primary" rounded size="sm">
+                      <MDBBtn onClick={toggleShow} key={product._id} color="primary" rounded size="sm">
                       <i class="fa-solid fa-pen-to-square"></i>
                       </MDBBtn>
+                      {toggleShow && <ProductEditModal key={product._id}/>}
+                      <MDBModal
+        tabIndex="-1"
+        show={fullscreenXlModal}
+        setShow={setFullscreenXlModal}
+      >
+        <MDBModalDialog size="fullscreen-lg-down">
+          <MDBModalContent>
+            <MDBModalHeader>
+              <MDBModalTitle className="text-success">
+                Modifica Prodotto {product.product}
+              </MDBModalTitle>
+              <MDBBtn
+                type="button"
+                className="btn-close"
+                color="none"
+                onClick={toggleShow}
+              ></MDBBtn>
+            </MDBModalHeader>
+            <MDBModalBody className="text-primary">
+            <form>
+      <MDBInput onChange={(e) =>
+                setNewProductForm({
+                  ...newProductFrom,
+                  product: e.target.value,
+                })
+              } className="text-primary" wrapperClass='mb-4' id='form6Example3' label='Prodotto' />
+      <MDBInput onChange={(e) =>
+                setNewProductForm({
+                  ...newProductFrom,
+                  thumbnail: e.target.value,
+                })
+              } wrapperClass='mb-4' id='typeURL' type='url' label='URL Immagine' />
+      <MDBInput  onChange={(e) =>
+                setNewProductForm({
+                  ...newProductFrom,
+                  category: e.target.value,
+                })
+              } className="text-primary" wrapperClass='mb-4' type='email' id='form6Example5' label='Categoria' />
+      <MDBInput onChange={(e) =>
+                setNewProductForm({
+                  ...newProductFrom,
+                  price: e.target.value,
+                })
+              } className="text-primary" wrapperClass='mb-4' type='tel' id='form6Example6' label='Prezzo' />
+
+      <MDBInput onChange={(e) =>
+                setNewProductForm({
+                  ...newProductFrom,
+                  description: e.target.value,
+                })
+              } className="text-primary" wrapperClass='mb-4' textarea id='form6Example7' rows={4} label='Aggiungi una descrizione' />
+    </form>
+            </MDBModalBody>
+            <MDBModalFooter>
+              <MDBBtn className="mx-2" color="success">
+                Modifica
+              </MDBBtn>
+            </MDBModalFooter>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
+
+
                     </td>
                     <td>
                       <MDBBtn color="danger" rounded size="sm">
